fa0682c43ddb0b8f3bc5b18dc4379b06d3bd38e7
Artem Rakov <scoff1k@gmail.com>
2019-06-25 11:43:31 +0300

    Init

---
+++ config/environment.rb
@@ -0,0 +1,49 @@
+require 'bundler/setup'
+require 'hanami/setup'
+require 'hanami/model'
+require_relative '../lib/omniscrapper_hq'
+require_relative '../apps/web/application'
+
+Hanami.configure do
+  mount Web::Application, at: '/'
+
+  model do
+    ##
+    # Database adapter
+    #
+    # Available options:
+    #
+    #  * SQL adapter
+    #    adapter :sql, 'sqlite://db/omniscrapper_hq_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/omniscrapper_hq_development'
+    #    adapter :sql, 'mysql://localhost/omniscrapper_hq_development'
+    #
+    adapter :sql, ENV.fetch('DATABASE_URL')
+
+    ##
+    # Migrations
+    #
+    migrations 'db/migrations'
+    schema     'db/schema.sql'
+  end
+
+  mailer do
+    root 'lib/omniscrapper_hq/mailers'
+
+    # See http://hanamirb.org/guides/mailers/delivery
+    delivery :test
+  end
+
+  environment :development do
+    # See: http://hanamirb.org/guides/projects/logging
+    logger level: :debug
+  end
+
+  environment :production do
+    logger level: :info, formatter: :json, filter: []
+
+    mailer do
+      delivery :smtp, address: ENV.fetch('SMTP_HOST'), port: ENV.fetch('SMTP_PORT')
+    end
+  end
+end
