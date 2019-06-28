9d134078ab8106934c426592b28d79a45e01ebc5
Artem Rakov <scoff1k@gmail.com>
2019-06-28 10:27:24 +0300

    added test for create

--- spec/web/controllers/sites/create_spec.rb
+++ spec/web/controllers/sites/create_spec.rb
@@ -1,10 +1,41 @@
-# RSpec.describe Web::Controllers::Sites::Create, type: :action do
-#   let(:action) { described_class.new }
-#   let(:site) { Fabricate(:site) }
-#   let(:params) { { id: site.id } }
-#
-#   it 'is successful' do
-#     response = action.call(params)
-#     expect(response[0]).to eq 302
-#   end
-# end
+RSpec.describe Web::Controllers::Sites::Create, type: :action do
+  let(:action) { described_class.new }
+  let(:repository) { SiteRepository.new }
+
+  before do
+    repository.clear
+  end
+  context 'with valid params' do
+    let(:attributes) { Fabricate.attributes_for(:site) }
+
+    it 'is successful' do
+      response = action.call(site: attributes)
+      expect(response[0]).to eq 302
+    end
+
+    it 'creates a new site' do
+      action.call(site: attributes)
+      site = repository.last
+
+      expect(site.id).to_not be_nil
+    end
+  end
+
+  context 'with invalid params' do
+    let(:attributes) { { site: {} } }
+
+    it 'returns HTTP client error' do
+      response = action.call(attributes)
+
+      expect(response[0]).to eq(422)
+    end
+
+    it 'dumps errors in params' do
+      action.call(attributes)
+      errors = action.params.errors
+
+      expect(errors.dig(:site, :name)).to eq(['is missing'])
+      expect(errors.dig(:site, :url)).to eq(['is missing'])
+    end
+  end
+end
