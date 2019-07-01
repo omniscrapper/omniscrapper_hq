c66e3e91f2502517cddd20c3cf097d41d6f94f3b
Artem Rakov <scoff1k@gmail.com>
2019-07-01 14:45:00 +0300

    added assosiation to site repository

--- lib/omniscrapper_hq/repositories/site_repository.rb
+++ lib/omniscrapper_hq/repositories/site_repository.rb
@@ -1,2 +1,13 @@
 class SiteRepository < Hanami::Repository
+  associations do
+    has_many :schemas
+  end
+
+  def create_with_schemas(data)
+    assoc(:schemas).create(data)
+  end
+
+  def find_with_schemas(id)
+    aggregate(:schemas).where(id: id).as(Site).one
+  end
 end
