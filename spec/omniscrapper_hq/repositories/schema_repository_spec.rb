dc026f2947ca58a3429a9dcbae6d82b048855e99
Artem Rakov <scoff1k@gmail.com>
2019-07-01 17:01:03 +0300

    added tests for schema repository

--- spec/omniscrapper_hq/repositories/schema_repository_spec.rb
+++ spec/omniscrapper_hq/repositories/schema_repository_spec.rb
@@ -1,3 +1,20 @@
 RSpec.describe SchemaRepository, type: :repository do
-  # place your tests here
+  let(:repository) { described_class.new }
+  let(:schema) { Fabricate(:schema) }
+  context '.find_with_site' do
+    it 'finds schema with site' do
+      schema_with_site = repository.find_with_site(schema.id)
+      expect(schema_with_site.site).to_not be_nil
+    end
+
+    it 'returns nil when nil is given' do
+      found = repository.find_with_site(nil)
+      expect(found).to be_nil
+    end
+
+    it 'returns nil for missing record' do
+      found = repository.find_with_site('9999999')
+      expect(found).to be_nil
+    end
+  end
 end
