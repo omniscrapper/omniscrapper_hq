f626b7833e5087195581a5bc6a320ffbd320809b
Stanislav Mekhonoshin <ejabberd@gmail.com>
2019-11-04 15:44:51 +0300

    Add output to seeds

--- rakelib/seeds.rb
+++ rakelib/seeds.rb
@@ -2,20 +2,36 @@ class Seeds
   include Import[
     task_repo: 'repositories.task',
     schema_repo: 'repositories.schema',
-    site_repo: 'repositories.site'
+    site_repo: 'repositories.site',
+    output_repo: 'repositories.output',
+    test_result_repo: 'repositories.test_result'
   ]
 
   def call(repos = {})
     @repos = repos
+    clean_database
     seed_all
   end
 
   private
 
+  def clean_database
+    [
+      test_result_repo,
+      task_repo,
+      schema_repo,
+      site_repo,
+      output_repo
+    ].each do |repo|
+      repo.clear
+    end
+  end
+
   def seed_all
     site = seed_sites
+    output = seed_outputs
     schema = seed_schemas
-    seed_tasks(site, schema)
+    seed_tasks(site, schema, output)
   end
 
   def seed_sites
@@ -25,6 +41,16 @@ class Seeds
     )
   end
 
+  def seed_outputs
+    output_repo.create(
+      adapter: 'Kafka',
+      adapter_params: {
+        kafka_host: 'localhost',
+        kafka_topic: 'scrapping'
+      }
+    ) 
+  end
+
   def seed_schemas
     schema_repo.create(
       name: 'real estate ads',
@@ -32,10 +58,11 @@ class Seeds
     )
   end
 
-  def seed_tasks(site, schema)
+  def seed_tasks(site, schema, output)
     task_repo.create(
       site_id: site.id,
       schema_id: schema.id,
+      output_id: output.id,
       crawler: 'gallery',
       crawler_params: crawler_params,
       scrapper_params: scrapper_params
