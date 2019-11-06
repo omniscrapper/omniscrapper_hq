0e0e42722bfb6ef6aac457700286a62c1381dde3
Oleg Pervushin <alec.ext@gmail.com>
2019-11-06 22:27:16 +0400

    fixed output/index_spec.rb

--- spec/web/controllers/outputs/index_spec.rb
+++ spec/web/controllers/outputs/index_spec.rb
@@ -1,9 +1,22 @@
 RSpec.describe Web::Controllers::Outputs::Index, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
+  let(:output) { Fabricate.build(:output) }
+  let(:output_repo) { double 'Output' }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  before do
+    allow(output_repo).to receive(:all).and_return([output])
+    System::Container.stub('repositories.output', output_repo)
+    @response = action.call(params)
+  end
+
+  it 'should be 200 http status' do
+    expect(@response.first).to eq 200
+  end
+
+  context 'exposures' do
+    it 'should return existed outputs' do
+      expect(action.exposures.fetch(:outputs)).to match_array([output])
+    end
   end
 end
