4d6500200b103cc3ac77f0586e514978e6c5ae45
Oleg Pervushin <alec.ext@gmail.com>
2019-11-06 22:12:43 +0400

    fixed dry_run/create_spec.rb

--- spec/web/controllers/dry_run/create_spec.rb
+++ spec/web/controllers/dry_run/create_spec.rb
@@ -1,14 +1,10 @@
 RSpec.describe Web::Controllers::DryRun::Create, type: :action do
   let(:action) { described_class.new }
 
-  before do
-    @response = action.call(params)
-  end
-
   describe 'if a task is found' do
     let(:task) { Fabricate.build(:task) }
     let(:params) { { task_id: 1 } }
-    let(:task_repo) { TaskRepository.new }
+    let(:task_repo) { double 'TaskRepository' }
 
     before do
       allow(task_repo).to receive(:find).and_return(task)
@@ -16,20 +12,43 @@ RSpec.describe Web::Controllers::DryRun::Create, type: :action do
     end
 
     it 'should be 200 http status' do
-      expect(@response.first).to eq 200
+      response = action.call(params)
+
+      expect(response.first).to eq 200
     end
 
     context 'exposures' do
       it 'should return the task' do
+        action.call(params)
+
         expect(action.exposures.fetch(:task)).to eq task
       end
     end
 
-    context 'if required params is sent' do
-      it 'should return flash message'
+    context 'if optional params is sent' do
+      let(:params) do
+        { task_id: 1, dry_run: { url: Faker::Internet.url } }
+      end
 
       context 'exposures' do
-        it 'should return the result'
+        let(:flash_text) { 'Dry run was successfully performed.' }
+        let(:result) { double 'Operations::DryRun' }
+        let(:another_task) { Fabricate.build(:task) }
+
+        before do
+          allow(result).to receive(:call).and_return(another_task)
+          System::Container.stub('operations.dry_run.perform', result)
+          action.call(params)
+        end
+
+        it 'should return flash message' do
+          expect(action.exposures.fetch(:flash)[:success]).
+            to eq flash_text
+        end
+
+        it 'should return the result' do
+          expect(action.exposures.fetch(:result)).to eq task
+        end
       end
     end
   end
